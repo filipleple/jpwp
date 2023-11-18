4e16c630a2291723ea828e35701dae9afa2c97ea
Filip <filiple00@protonmail.com>
2023-11-19 00:58:16 +0100

    player.getCollisionType(): more elaborate collision detection, useless ATM

--- jpwp/Player.cs
+++ jpwp/Player.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Forms;
 
+enum collisionTypes
+{
+    TOP=0,
+    LEFT,
+    RIGHT,
+    BOTTOM
+} 
+
 
 namespace jpwp
 {
@@ -17,6 +25,8 @@ namespace jpwp
 
         public bool goLeft, goRight, jumping, inAirNoCollision;
         int force, horizontalSpeed, jumpSpeed, gravity;
+
+        List<bool> collisions = new List<bool>();
         
         System.Drawing.Color color;
 
@@ -27,7 +37,8 @@ namespace jpwp
             rect.X = xpos;
             rect.Y = ypos;            
 
-            Console.WriteLine("jumping: " + jumping + " force: " + force + " y speed: " + horizontalSpeed + " nocollision: " + inAirNoCollision);
+            // jump params debug dump
+            //Console.WriteLine("jumping: " + jumping + " force: " + force + " y speed: " + horizontalSpeed + " nocollision: " + inAirNoCollision);
 
             // collision-independent segment (x axis)
             if (goLeft == true)
@@ -40,22 +51,40 @@ namespace jpwp
             }
 
             inAirNoCollision = true;
+            collisions = new List<bool> { false, false, false, false };
+
             foreach (Platform platform in platforms)
             {
                 if (rect.IntersectsWith(platform.rect))                
-                {
+                {                    
                     inAirNoCollision = false;
+
                     if (!jumping)
                     {
                         ypos = platform.ypos - height+1;
                     }
-                }                
+
+                    // more complex collision detection
+                    //collisions = getCollisionType(rect, platform.rect);
+                    /*
+                    Console.WriteLine(
+                        "TOP: " + collisions[(int)collisionTypes.TOP] +
+                        " RIGHT: " + collisions[(int)collisionTypes.RIGHT] +
+                        " BOTTOM: " + collisions[(int)collisionTypes.BOTTOM] +
+                        " LEFT: " + collisions[(int)collisionTypes.LEFT]
+                    );
+                    */
+
+
+                }
             }
+
 
             // collision-dependent segment (y axis)
             if (inAirNoCollision && !jumping)
             {
                 ypos += gravity;
+                //try to replace with only bottom collision 
             }
 
             if (jumping)
@@ -100,6 +129,42 @@ namespace jpwp
             this.gravity = GlobalConfig.PLAYER_GRAVITY;
 
             this.color = GlobalConfig.PLAYER_COLOR;
+        }
+
+        public List<bool> getCollisionType(Rect player, Rect platform)
+        {
+            List<bool> result = new List<bool>() { false, false, false, false };
+
+            // Top collision
+            Rect underPlatform = new Rect(platform.X, platform.Y + platform.Height, platform.Width, GlobalConfig.SCREEN_HEIGHT - platform.Height);
+            if (underPlatform.IntersectsWith(player))
+            {
+                result[(int)collisionTypes.TOP] = true;
+            }
+
+            // Right collision
+            Rect leftOfPlatform = new Rect(0, platform.Y, platform.X, platform.Height);
+            if (leftOfPlatform.IntersectsWith(player))
+            {
+                result[(int)collisionTypes.RIGHT] = true;
+            }
+
+            // Bottom collision
+            Rect abovePlatform = new Rect(platform.X, 0, platform.Width, platform.Y);
+            if (abovePlatform.IntersectsWith(player))
+            {
+                result[(int)collisionTypes.BOTTOM] = true;
+            }
+
+            // Left collision
+            Rect rightOfPlatform = new Rect(platform.X + platform.Width, platform.Y, GlobalConfig.SCREEN_WIDTH - platform.X - platform.Width, platform.Height);
+            if (rightOfPlatform.IntersectsWith(player))
+            {
+                result[(int)collisionTypes.LEFT] = true;
+            }
+                        
+
+            return result;
         }
     }
 }
